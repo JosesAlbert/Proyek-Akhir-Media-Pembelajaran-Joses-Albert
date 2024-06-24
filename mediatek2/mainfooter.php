d772f687e1698ce463834012b28098b3eccc6dc6
JosesAlbert <131852184+JosesAlbert@users.noreply.github.com>
2024-06-24 16:52:36 +0800

    Add files via upload
    Proyek

---
+++ mediatek2/mainfooter.php
@@ -0,0 +1,6 @@
+		<div id="footer">
+			<STRONG>DEVELOPED by</STRONG> : <a href="">ekanur</a>, <a href="">gayan</a>, <a href="">Yara</a> | Inspired from <strong>FunEnglish SMKN 4 Malang</strong>
+        </div>
+	</div>
+	</body>
+</html>
---
+++ mediatek2/mainheader.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Media Pembelajaran-Komputer & Jaringan Komputer</title>
+
+<link href="./assets/css/style.css" rel="stylesheet"/>
+<link href="./assets/css/bootstrap.css" rel="stylesheet"/>
+<script src="assets/js/jquery-2.0.3.min.js" ></script>
+<script src="assets/js/bootstrap.js" ></script>
+<!-- <script src="assets/js/page.js" ></script> -->
+</head>
+<body>
+
+<!-- <div class="headmenu">
+    <a href="index.php" class="full"><div class="btn">HOME</div></a>
+    <a href="menu_learn.php" class="full"><div class="btn">LEARN</div></a>
+    <a href="menu_exercise.php" class="full"><div class="btn">EXERCISE</div></a>
+    
+</div> -->
+<div id="container">
